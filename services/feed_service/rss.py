0d254c71abfb499759729dbd5d3e8fc51e4a36aa
Morosanu Dan <danutmoro@yahoo.com>
2017-01-31 23:26:52 +0200

    Update rss.py

--- services/feed_service/rss.py
+++ services/feed_service/rss.py
@@ -236,9 +236,7 @@ def feeder():
     if 'user_id' not in request.json or 'keywords' not in request.json:
         abort(422)  # The 422 (Unprocessable Entity) status code means the server understands the content type of the request entity (hence a 415(Unsupported Media Type) status code is inappropriate), and the syntax of the request entity is correct (thus a 400 (Bad Request) status code is inappropriate) but was unable to process the contained instructions. For example, this error condition may occur if an XML request body contains well-formed (i.e., syntactically correct), but semantically erroneous, XML instructions.
 
-    user_id = int(request.json['user_id'])
-
-    return feed_articles(user_id, request.json["keywords"])
+    return feed_articles(request.json['user_id'], request.json["keywords"])
 
 
 if __name__ == "__main__":
